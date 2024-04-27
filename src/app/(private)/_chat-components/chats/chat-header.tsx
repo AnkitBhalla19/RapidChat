7a66f4bc16d65b5cafe1e2806d369e97c171f1aa
AnkitBhalla19 <94590018+AnkitBhalla19@users.noreply.github.com>
2024-04-27 11:22:48 +0530

    antd dropdown changes

--- src/app/(private)/_chat-components/chats/chat-header.tsx
+++ src/app/(private)/_chat-components/chats/chat-header.tsx
@@ -1,35 +1,48 @@
-
+import { Button, Dropdown, MenuProps, Space } from "antd";
 import React from 'react'
 import NewChatModal from './new-chat-modal';
 import { useRouter } from 'next/navigation';
-import { Dropdown ,DropdownItem,DropdownMenu,Button,DropdownTrigger} from '@nextui-org/react';
+import { DownOutlined } from '@ant-design/icons';
+// import { Dropdown ,DropdownItem,DropdownMenu,Button,DropdownTrigger} from '@nextui-org/react';
 
 function ChatHeader() {
     const router = useRouter();
+    const handleMenuClick: MenuProps['onClick'] = (e) => {
+        console.log('click', e);
+      };
 
     const [showNewModal, setShowNewModal] = React.useState(false)
+    const items: MenuProps["items"] = [
+        {
+          label: "New Chat",
+          key: "1",
+          onClick: () => setShowNewModal(true),
+        },
+        {
+          label: "New Group",
+          key: "2",
+          onClick: () => router.push("/groups/create-group"),
+        },
+      ];
+
+      const menuProps = {
+        items,
+        onClick: handleMenuClick,
+      };
 
   return (
     <div>
     <div className='flex  justify-between items-center'>
-        <div className='text-xl font-bold uppercase text-gray-500'
+        <div className='text-xl font-bold uppercase text-primary'
         >My Chats</div>
-        <Dropdown>
-            <DropdownTrigger>
-                <Button>
-                    New
-                </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-            aria-label='Dropdown varient'
-            color='primary'
-            variant='solid'
-            >
-                <DropdownItem key="1" onClick={()=>setShowNewModal(true)}>New Chat</DropdownItem>
-                <DropdownItem key="2" onClick={()=>router.push('/groups/create-group')}>New Group</DropdownItem>
-            </DropdownMenu>
-
-        </Dropdown>
+            <Dropdown menu={menuProps}>
+      <Button className="bg-yellow-500">
+        <Space>
+          <span className="text-primary font-bold uppercase">New</span>
+          <DownOutlined />  
+        </Space>
+      </Button>
+    </Dropdown>
         </div>
         <input type="text" placeholder='Search' className='bg-yellow-500/60 w-full border border-primary border-solid rounded-md px-3 h-14 mt-3 mb-3 outline-none focus:outline-none focus:border-primary text-primary'/>
         {
